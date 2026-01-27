d441750b2619a09ba7045018332972438765196a
issuejong <lsj63223@naver.com>
2026-01-27 14:10:21 +0900

    chore: 초기 화면 설정

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import initial_view
 
 urlpatterns = [
+    
+    path("", initial_view.as_view(), name="initial"),
     path("admin/", admin.site.urls),
 
     # allauth (로그인/소셜로그인)
