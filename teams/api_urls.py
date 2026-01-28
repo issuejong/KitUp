152ad972445d0375d40a5e7f45e79df035ef9567
issuejong <lsj63223@naver.com>
2026-01-28 17:03:33 +0900

    feat: teams swagger용 api crud 기능 구현

--- teams/api_urls.py
+++ teams/api_urls.py
@@ -1,4 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TeamViewSet, TeamMemberViewSet
+
+router = DefaultRouter()
+router.register(r'teams', TeamViewSet, basename='team')
+router.register(r'team-members', TeamMemberViewSet, basename='team-member')
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
