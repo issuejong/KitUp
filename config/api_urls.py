c6c45e776b24acbfd85463fd413d82a938cd4d19
issuejong <lsj63223@naver.com>
2026-01-31 13:47:01 +0900

    feat: 이메일 인증 회원가입 구현

--- config/api_urls.py
+++ config/api_urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("apps.accounts.api_urls")),
-    path("", include("apps.projects.api_urls")),
-    path("", include("apps.teams.api_urls")),
-    path("", include("apps.guides.api_urls")),
-    path("", include("apps.reflections.api_urls")),
+    path("accounts/", include("apps.accounts.api_urls")),
+    path("projects/", include("apps.projects.api_urls")),
+    path("teams/", include("apps.teams.api_urls")),
+    path("guides/", include("apps.guides.api_urls")),
+    path("reflections/", include("apps.reflections.api_urls")),
 ]
