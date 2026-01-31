ccbcf460b0e0329e43cd92d08756af430f6caea7
issuejong <lsj63223@naver.com>
2026-01-31 20:55:38 +0900

    fix: import 오류 해결 및 변수명 통일

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,5 @@
-from datetime import timezone
+from django.urls import reverse
+from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
@@ -66,7 +67,7 @@ def level_submit(request):
     if request.method != "POST":
         return HttpResponseBadRequest("잘못된 요청입니다.")
     
-    role_code = request.POST.get("role_code")
+    role_code = request.POST.get("role")
     role = get_object_or_404(Role, code=role_code)
     level = request.POST.get("level")
     
@@ -79,7 +80,7 @@ def level_submit(request):
         },
     )
     
-    return redirect("test:test_result") + f"?role={role_code}"
+    return redirect(f"{reverse('accounts:test_result')}?role={role_code}")
 
 @login_required
 def test_result(request):
@@ -104,7 +105,7 @@ def test_result(request):
         "role": role,  # role.name 출력 가능
         "level": url_level.level if url_level else None,
     }
-    return render(request, "test/test_result.html", context)
+    return render(request, "accounts/test_result.html", context)
 
 
 @login_required
