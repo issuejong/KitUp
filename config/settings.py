46ccfff5f96555f704d8c113b602dc29c1d656ff
bimvocado <labeou@naver.com>
2026-02-01 21:16:03 +0900

    fix: crsf 인증

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['kitup.duckdns.org', '3.37.88.175', 'localhost', '127.0.0.1',]
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://kitup.duckdns.org',
+    'http://kitup.duckdns.org:8000',
+    'https://kitup.duckdns.org',
+]
 
 # Application definition
 
