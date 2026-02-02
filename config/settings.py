cf9b3a7a787a2ad5a92de2416bb77af3c0678bd2
issuejong <lsj63223@naver.com>
2026-02-02 23:00:59 +0900

    feat: 이메일 내용 설정

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,7 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "KITUP")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", '"KITUP" <noreply@kitup.com>')
 
 # 비밀번호 재설정
 ACCOUNT_PASSWORD_RESET_ON_CHANGE = False  # 비밀번호 변경 시 재로그인 불필요
