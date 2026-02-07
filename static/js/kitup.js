45465305ada0bd058bd9025fe4aa19a5c87bb01a
Tonyjoo11 <hyungjoo.ree@gmail.com>
2026-02-07 15:01:56 +0900

    feat: kitup_detail.html 완성. 즐겨찾기를 서버와 동기화를 위한 TODO 추가

--- static/js/kitup.js
+++ static/js/kitup.js
@@ -1,8 +1,10 @@
 // static/js/kitup.js
+
 document.addEventListener("click", (e) => {
   const btn = e.target.closest("[data-like-btn]");
   if (!btn) return;
 
+  // 카드 <a> 클릭 방지
   e.preventDefault();
   e.stopPropagation();
 
